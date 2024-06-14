d625b96f227b107ae223a369078ed0198a127ce1
Andrew <a.slepakurov@gmail.com>
2024-06-14 22:18:13 +0300

    add vpc, network (#4)

---
+++ modules/network/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "The AWS region to spin infra in"
+}
+
+variable "aws_project" {
+  description = "Top level project name"
+}
+
+variable "az_count" {
+  description = "Number of AZs to cover in a given AWS region"
+  default     = "2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR for the VPC"
+  default     = "172.17.0.0/16"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "AWS tags"
+}
