8954263d2d0b455717e889992b32bfd20fd95fe6
aslepakurov <a.slepakurov@gmail.com>
2024-06-14 22:02:59 +0300

    add var vpc_cidr

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -10,3 +10,8 @@ variable "az_count" {
   description = "Number of AZs to cover in a given AWS region"
   default     = "2"
 }
+
+variable "vpc_cidr" {
+  description = "CIDR for the VPC"
+  default     = "172.17.0.0/16"
+}
