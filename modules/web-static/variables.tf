c574fd7404ee986a0eaf3b9138e524ccdd4a3780
Andrew <a.slepakurov@gmail.com>
2024-06-29 11:49:37 +0300

    updated bucket policies (#8)

--- modules/web-static/variables.tf
+++ modules/web-static/variables.tf
@@ -6,6 +6,11 @@ variable "aws_region" {
   type = string
 }
 
+variable "default_page" {
+  type    = string
+  default = "index.html"
+}
+
 variable "tags" {
   type        = map(string)
   description = "AWS tags"
