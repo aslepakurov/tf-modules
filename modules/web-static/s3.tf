12095d8a159f69f9f3765be5566537e13e9c5d52
aslepakurov <a.slepakurov@gmail.com>
2024-06-29 17:24:24 +0300

    fix acl

--- modules/web-static/s3.tf
+++ modules/web-static/s3.tf
@@ -5,27 +5,10 @@ resource "aws_s3_bucket" "s3_bucket_static" {
   tags = var.tags
 }
 
-resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.s3_bucket_static.id
-
-  rule {
-    object_ownership = "BucketOwnerEnforced"
-  }
-}
-
 resource "aws_s3_bucket_acl" "admin_site_acl" {
   bucket = aws_s3_bucket.s3_bucket_static.id
 
   acl = "public-read"
-}
-
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.s3_bucket_static.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_website_configuration" "this" {
@@ -38,22 +21,4 @@ resource "aws_s3_bucket_website_configuration" "this" {
   error_document {
     key = var.error_page
   }
-}
-
-resource "aws_s3_bucket_policy" "this" {
-  bucket = aws_s3_bucket.s3_bucket_static.id
-
-  policy = jsonencode({
-    Version   = "2012-10-17"
-    Id        = "AllowGetObjects"
-    Statement = [
-      {
-        Sid       = "AllowPublic"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.s3_bucket_static.arn}/**"
-      }
-    ]
-  })
 }
