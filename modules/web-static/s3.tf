adf55c090d20e8538ba1c15ac81dd1d3a5297a4c
aslepakurov <a.slepakurov@gmail.com>
2024-06-29 17:32:17 +0300

    change acl enforcement

--- modules/web-static/s3.tf
+++ modules/web-static/s3.tf
@@ -8,7 +8,16 @@ resource "aws_s3_bucket" "s3_bucket_static" {
 resource "aws_s3_bucket_acl" "admin_site_acl" {
   bucket = aws_s3_bucket.s3_bucket_static.id
 
-  acl = "public-read"
+  acl = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.s3_bucket_static.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_website_configuration" "this" {
@@ -21,4 +30,22 @@ resource "aws_s3_bucket_website_configuration" "this" {
   error_document {
     key = var.error_page
   }
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.s3_bucket_static.id
+
+  policy = jsonencode({
+    Version   = "2012-10-17"
+    Id        = "AllowGetObjects"
+    Statement = [
+      {
+        Sid       = "AllowPublic"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.s3_bucket_static.arn}/**"
+      }
+    ]
+  })
 }
