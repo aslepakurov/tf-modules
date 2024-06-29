facb965d1df2558da5bd0ba64b0d1b42c0639250
aslepakurov <a.slepakurov@gmail.com>
2024-06-29 11:48:15 +0300

    updated bucket policies

--- modules/web-static/s3.tf
+++ modules/web-static/s3.tf
@@ -5,20 +5,24 @@ resource "aws_s3_bucket" "s3_bucket_static" {
   tags = var.tags
 }
 
-resource "aws_s3_bucket_acl" "s3_bucket_acl" {
+resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.s3_bucket_static.id
-  acl    = "private"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_website_configuration" "this" {
   bucket = aws_s3_bucket.s3_bucket_static.bucket
 
   index_document {
-    suffix = "index.html"
+    suffix = var.default_page
   }
 
   error_document {
-    key = "index.html"
+    key = var.default_page
   }
 }
 
