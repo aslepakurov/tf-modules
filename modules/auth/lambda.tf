11b744f21af96066f0bd24e23b2b4dd71ae8c0c6
aslepakurov <a.slepakurov@gmail.com>
2025-08-18 00:07:46 +0300

    update permissions

--- modules/auth/lambda.tf
+++ modules/auth/lambda.tf
@@ -151,11 +151,19 @@ resource "aws_lambda_function" "post_confirmation" {
 # Lambda trigger is now configured directly in the Cognito user pool resource
 
 # Permission for Cognito to invoke the Lambda function
-resource "aws_lambda_permission" "allow_cognito" {
+resource "aws_lambda_permission" "allow_cognito_invoke" {
   count         = local.create_lambda ? 1 : 0
   statement_id  = "AllowExecutionFromCognito"
-  # action        = "lambda:InvokeFunction"
-  action        = "lambda:*"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.post_confirmation[0].function_name
+  principal     = "cognito-idp.amazonaws.com"
+  source_arn    = aws_cognito_user_pool.user_pool.arn
+}
+
+resource "aws_lambda_permission" "allow_cognito_create" {
+  count         = local.create_lambda ? 1 : 0
+  statement_id  = "AllowExecutionFromCognito"
+  action        = "lambda:CreateFunction"
   function_name = aws_lambda_function.post_confirmation[0].function_name
   principal     = "cognito-idp.amazonaws.com"
   source_arn    = aws_cognito_user_pool.user_pool.arn
